f0404c5be83cf78b1ba0f33b5fbefc6022a94f5d
Hyeyoung-Eum <djagpdud125@korea.ac.kr>
2023-05-14 22:40:20 +0900

    그룹단어

---
+++ python/1316-python.py
@@ -0,0 +1,15 @@
+#그룹단어
+N=int(input())
+cnt=0
+for i in range(N):
+    word = input()
+    bl_=True
+
+    for j in range(len(word)-1):
+        if word[j] in word[j+1:]:
+            bl_=False
+            break
+    if bl_:
+        cnt+=1
+
+print(cnt)
