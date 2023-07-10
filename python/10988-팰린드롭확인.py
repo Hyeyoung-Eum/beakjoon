ff68bb7b82dbef2150d0db2bd3ff84e65f93e60e
Hyeyoung-Eum <djagpdud125@korea.ac.kr>
2023-07-10 23:56:31 +0900

    230710: 펠린드롭

--- python/10988-팰린드롭확인.py
+++ python/10988-팰린드롭확인.py
@@ -1,11 +1,18 @@
-word=input()
-word_list=list(word)
-b_list=[]
-for i in range(round(len(word_list)/2)):
-    if word_list[i] == word_list[len(word_list)-i-1]:
-        b_list.append(True)
+# word=input()
+# word_list=list(word)
+# b_list=[]
+# for i in range(round(len(word_list)/2)):
+#     if word_list[i] == word_list[len(word_list)-i-1]:
+#         b_list.append(True)
 
-if  len(b_list) == round(len(word_list)/2):
+# if  len(b_list) == round(len(word_list)/2):
+#     print(1)
+# else:
+#     print(0)
+
+word=list(input())
+
+if list(reversed(word)) == word:
     print(1)
 else:
     print(0)
