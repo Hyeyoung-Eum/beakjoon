6dc5605e15f04c557753897933ceb1378d4d0353
Hyeyoung-Eum <djagpdud125@korea.ac.kr>
2023-07-18 23:35:26 +0900

    230718: deque 풀이 추가

--- python/11866요세푸스문제0.py
+++ python/11866요세푸스문제0.py
@@ -1,3 +1,4 @@
+###첫 번째 queue를 활용한 풀이###
 n, k = map(int, input().split())
 
 #요세푸스 순열 생성
@@ -11,3 +12,22 @@ while queue:
     res.append(str(queue.pop(idx)))
 
 print('<', ', '.join(res), '>', sep='')
+
+###두 번째 deque를 활용한 풀이###
+# from collections import deque
+
+# n,k=map(int, input().split())
+
+# #양방향 연결 리스트(deque) 생성
+# deq = deque([i for i in range(1, n+1)])
+
+# #요세푸스 순열 생성
+# res=[]
+# while len(deq) != 0:
+#     for _ in range(k-1):
+#         #k-1번째 노드까지 deq 맨 뒤로 이동
+#         deq.append(deq.popleft())
+#     #k번째 노드 삭제 후 결과 배열에 추가
+#     res.append(str(deq.popleft()))
+# #결과 출력
+# print('<'+', '.join(res)+'>')
