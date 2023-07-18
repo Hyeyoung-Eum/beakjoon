30faf3ad53f0c5db7c85df2861671ae237953faf
Hyeyoung-Eum <djagpdud125@korea.ac.kr>
2023-07-18 23:29:55 +0900

    230718 queue를 활용한 문제

---
+++ python/11866요세푸스문제0.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+
+#요세푸스 순열 생성
+idx=0
+queue= [i for i in range(1, n+1)]
+res = []
+while queue:
+    idx += k-1 #k-1번째 인덱스까지 건너 뛰기
+    if idx >= len(queue): #길이와 같아지는 부분부터 인덱스를 초과하기 때문
+        idx %= len(queue) #idx = idx % len(queue)
+    res.append(str(queue.pop(idx)))
+
+print('<', ', '.join(res), '>', sep='')
