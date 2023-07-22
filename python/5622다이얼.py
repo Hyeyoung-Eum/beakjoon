2c34cbfe082326d375fb9849a52a1314467ff54f
Hyeyoung-Eum <djagpdud125@korea.ac.kr>
2023-07-23 00:07:55 +0900

    230723 다이얼 : 문자열 쪼개기는 list가 아닌 for로 처리

--- python/5622다이얼.py
+++ python/5622다이얼.py
@@ -1,22 +1,37 @@
-w_list=list(input())
+# w_list=list(input())
+
+# rst=0
+# for w in w_list:
+#     if w=='A' or w=='B' or w=='C':
+#         rst+=3
+#     elif w=='D' or w=='E' or w=='F':
+#         rst+=4
+#     elif w=='G' or w=='H' or w=='I':
+#         rst+=5
+#     elif w=='J' or w=='K' or w=='L':
+#         rst+=6
+#     elif w=='M' or w=='N' or w=='O':
+#         rst+=7
+#     elif w=='P' or w=='Q' or w=='R' or w=='S':
+#         rst+=8
+#     elif w=='T' or w=='U' or w=='V':
+#         rst+=9
+#     elif w=='W' or w=='X' or w=='Y' or w=='Z':
+#         rst+=10
+
+# print(rst)
+
+###2번째 방법 : list를 만들 필요가 없다 그냥 반복문으로 처리 ###
+
+dial_list=['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+w=input()
 
 rst=0
-for w in w_list:
-    if w=='A' or w=='B' or w=='C':
-        rst+=3
-    elif w=='D' or w=='E' or w=='F':
-        rst+=4
-    elif w=='G' or w=='H' or w=='I':
-        rst+=5
-    elif w=='J' or w=='K' or w=='L':
-        rst+=6
-    elif w=='M' or w=='N' or w=='O':
-        rst+=7
-    elif w=='P' or w=='Q' or w=='R' or w=='S':
-        rst+=8
-    elif w=='T' or w=='U' or w=='V':
-        rst+=9
-    elif w=='W' or w=='X' or w=='Y' or w=='Z':
-        rst+=10
+
+for unit in dial_list:
+    for i in unit:
+        for x in w:
+            if i == x:
+                rst += dial_list.index(unit) + 3
 
 print(rst)
