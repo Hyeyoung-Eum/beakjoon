73b667a8c91d47892b9cac119f660db8e5ccf0ff
Hyeyoung-Eum <djagpdud125@korea.ac.kr>
2023-08-25 03:42:24 +0900

    230825 3가지 방법으로 코딩 : rstript(), stack 개념 학습

--- python/4949균형잡힌세상.py
+++ python/4949균형잡힌세상.py
@@ -1,52 +1,45 @@
 #괄호 균형 판단 프로그램
 #(), []
 
+import sys
+input=sys.stdin.readline
+#readline은 공백도 받으므로, rstrip()로 공백 제거.
 while(True):
-    tf=0
-    line = input()
-    small=0#괄호가 열리면 +1, 닫히면 -1
-    large=0
+    tf=0 #오답이 확정일 때 -1로 구분지어주는 역할.
     
-    #문이 열릴 때마다,색을 칠해준다.
-    #닫을 때 칠해진 색과 닫을 색이 일치하지 않으면 break
-    #일치하면 그 색을 뽑아버린다.
-    color=[]
-    if len(line)==1 and line[-1]=='.':
+    line = input().rstrip() #입력 받기
+
+    #<괄호의 균형을 맞춰야하는 문제 해결 방법>
+        #가장 최근에 열린 괄호가, 자신의 짝과 일치하는지 확인한다.
+            #일치하지 않으면 break
+            #일치하면 그 색을 뽑아버린다.
+    stack=[]
+    
+    if len(line)==1 and line[-1]=='.':#.이 들어오면 정지
         break
+
     else:
         for i in line:
-            if i == '(':
-                small+=1
-                color.append('small')
+            if i == '(' or i=='[':
+                stack.append(i)
             elif i==')':
-                small-=1
-                if small <0: #한 번이라도 이런 경우가 생기면 에러처리를 위해 tf=-1로 변경.
-                    tf=-1
-                    break
-                if color[-1] !='small':
+                if len(stack)==0 or stack[-1] !='(':
                     tf=-1
                     break
                 else:
-                    color.pop()
-            elif i=='[':
-                large+=1
-                color.append('large')
+                    stack.pop()
             elif i==']':
-                large-=1
-                if large <0:
-                    tf=-1
-                    break
-                if color[-1] !='large':
+                if len(stack)==0 or stack[-1] !='[':
                     tf=-1
                     break
                 else:
-                    color.pop()
+                    stack.pop()
 
-    if small == 0 and large == 0 and tf==0:
+    if len(stack)==0 and tf!=-1:
         print('yes')
     else:
         print('no')
 
 #마지막에 .을 넣어도 끝나지 않음
     #=> while문 안의 for문이 아니라, for문 시작 전에 if break문을 넣어줬음.
-#print()를 해보면 no가 나옴.ㄴ
+#print()를 해보면 no가 나옴.
