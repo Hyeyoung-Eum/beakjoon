94bf73bfda8322e9284b9ad1e5b233152c6b8b6f
Hyeyoung-Eum <djagpdud125@korea.ac.kr>
2023-08-30 23:56:43 +0900

    230830 로직 변경 필요

--- python/1654랜선 자르기.py
+++ python/1654랜선 자르기.py
@@ -2,24 +2,41 @@ import sys, math
 input=sys.stdin.readline
 
 k, n= map(int, input().rstrip().split())
-#k개의 랜선
-#를 n개로 모두 같은 길이로 만드세요
+#k개의 랜선을 n개로 모두 같은 길이로 만드세요
 #n개를 만들 수 있는 랜선의 최대 길이 출력
 
-lines=[int(input()) for _ in range(k)]
+lines=[int(input().rstrip()) for _ in range(k)] #랜선 길이 입력 받기
 
 lines.sort(reverse=True) #오름차순으로 정리
     #lines는 내림차순 정렬 이후 건들이기 없기
 
 cnt=0 #몇 번째 순회인지 확인
+
+#다중반복문 탈출을 위한 불린변수 선언
 breaker1=False
 breaker2=False
+
+#핵심 코드 시작
 while(True):
-    if breaker1 == True and breaker2 == True: #삼중반복문 탈출
+    #part1.삼중반복문 탈출 코드
+    if breaker1 == True and breaker2 == True: 
         break
-    #밖에서 Lines를 변형해준다.
+    
+    #part2. 길이가 긴 랜선부터, (0.5)**cnt 배씩 곱해준 기준 길이를 만들기 위해 반복문 수행
+
     standard_lines = [math.floor(line*((0.5)**cnt)) for line in lines] #변경 가능한 조건
     for standard in standard_lines: #line은 나누는 기준 line
+        #ex) 입력받은 lines=[802, 743, 539, 457]의 경우 standard는 순서대로 이렇게 됨.
+            # stardard 802
+            # stardard 743
+            # stardard 539
+            # stardard 457
+            # stardard 401
+            # stardard 371
+            # stardard 269
+            # stardard 228
+            # stardard 200 ...
+
         #이중반복문 탈출
         if breaker1==True: 
             breaker2=True
@@ -34,3 +51,4 @@ while(True):
             breaker1=True
             break
     cnt+=1
+
