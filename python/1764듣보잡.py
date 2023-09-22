8aba284ce614c079497ad40db57782d017882efa
Hyeyoung-Eum <djagpdud125@korea.ac.kr>
2023-09-22 20:11:16 +0900

    230922 list, set연산으로 효율적으로 계산하기

--- python/1764듣보잡.py
+++ python/1764듣보잡.py
@@ -6,21 +6,17 @@ n, m = map(int, input().split())
 #n 듣도 못한 사람의 수
 #m 보도 못한 사람의 수
 
-people=[]
+people_who_dont_hear=[]
+people_who_dont_see=[]
 
-for i in range(n+m):
-    people.append(input())
-num=0
-find_people=[]
-while(len(people)!=0):
-    tem_people=people[0]
-    if people.count(tem_people)==2:
-        find_people.append(tem_people)
-        del people[0]
-        num+=1
-    else: #개수 하나
-        del people[0]
-print(num)
-find_people.sort()
-for j in find_people:
-    print(j)
+for i in range(n):
+    people_who_dont_hear.append(input())
+
+for j in range(n):
+    people_who_dont_see.append(input())
+
+intersection = list(set(people_who_dont_hear) & set(people_who_dont_see))
+intersection.sort()
+print(len(intersection))
+for i in intersection:
+    print(i)
